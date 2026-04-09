f3fb521892f30d1c3cd43738b3f8442e04753f31
aki <sylvia.aki.lxc@gmail.com>
2026-04-10 01:16:26 +0900

    Initial Commit

---
+++ Plum.B/Plum_BApp.swift
@@ -0,0 +1,17 @@
+//
+//  Plum_BApp.swift
+//  Plum.B
+//
+//  Created by aki on 2026/4/10.
+//
+
+import SwiftUI
+
+@main
+struct Plum_BApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Plum.BTests/Plum_BTests.swift
@@ -0,0 +1,17 @@
+//
+//  Plum_BTests.swift
+//  Plum.BTests
+//
+//  Created by aki on 2026/4/10.
+//
+
+import Testing
+@testable import Plum_B
+
+struct Plum_BTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
